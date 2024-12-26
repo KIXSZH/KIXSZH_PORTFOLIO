9415cfeb265080d2cc26d97213494bc8e363ea2e
KISHOREKUMAR A <120034555+KIXSZH@users.noreply.github.com>
2024-12-26 19:58:57 +0530

    back to normal with update

--- app/components/homepage/hero-section/index.jsx
+++ app/components/homepage/hero-section/index.jsx
@@ -4,14 +4,17 @@ import { personalData } from "@/utils/data/personal-data";
 import Image from "next/image";
 import Link from "next/link";
 import { BsGithub, BsLinkedin } from "react-icons/bs";
+import { FaFacebook, FaInstagramSquare, FaTwitterSquare } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa6";
-import { RiContactsFill, RiFileTextFill } from "react-icons/ri"; // Import required icons
+import { MdDownload } from "react-icons/md";
+import { RiContactsFill } from "react-icons/ri";
 import { SiLeetcode } from "react-icons/si";
+import { RiFileTextFill } from "react-icons/ri";
+
 
 function HeroSection() {
   return (
     <section className="relative flex flex-col items-center justify-between py-4 lg:py-12">
-      {/* Background Image */}
       <Image
         src="/hero.svg"
         alt="Hero"
@@ -21,83 +24,74 @@ function HeroSection() {
       />
 
       <div className="grid grid-cols-1 items-start lg:grid-cols-2 lg:gap-12 gap-y-8">
-        {/* Left Section */}
         <div className="order-2 lg:order-1 flex flex-col items-start justify-center p-2 pb-20 md:pb-10 lg:pt-10">
           <h1 className="text-3xl font-bold leading-10 text-white md:font-extrabold lg:text-[2.6rem] lg:leading-[3.5rem]">
             Hello, <br />
-            This is{" "}
-            <span className="text-pink-500">{personalData.name}</span>
+            This is {' '}
+            <span className=" text-pink-500">{personalData.name}</span>
             {` , I'm Passionate about `}
-            <span className="text-[#16f2b3]">{personalData.designation}</span>.
+            <span className=" text-[#16f2b3]">{personalData.designation}</span>
+            .
           </h1>
 
-          {/* Social Links */}
           <div className="my-12 flex items-center gap-5">
             <Link
               href={personalData.github}
-              target="_blank"
+              target='_blank'
               className="transition-all text-pink-500 hover:scale-125 duration-300"
             >
               <BsGithub size={30} />
             </Link>
             <Link
               href={personalData.linkedIn}
-              target="_blank"
+              target='_blank'
               className="transition-all text-pink-500 hover:scale-125 duration-300"
             >
               <BsLinkedin size={30} />
             </Link>
+           
             <Link
               href={personalData.leetcode}
-              target="_blank"
+              target='_blank'
               className="transition-all text-pink-500 hover:scale-125 duration-300"
             >
               <SiLeetcode size={30} />
             </Link>
             <Link
               href={personalData.insta}
-              target="_blank"
+              target='_blank'
               className="transition-all text-pink-500 hover:scale-125 duration-300"
             >
               <FaInstagram size={30} />
             </Link>
           </div>
 
-          {/* Action Buttons */}
           <div className="flex items-center gap-3">
-            {/* Contact Me Button */}
-            <Link
-              href="#contact"
-              className="bg-gradient-to-r to-pink-500 from-violet-600 p-[1px] rounded-full transition-all duration-300 hover:from-pink-500 hover:to-violet-600"
-            >
-              <button className="px-5 font-roboto-mono md:px-8 py-3 md:py-4 bg-[#0d1224] rounded-full border-none text-center md:text-base font-medium tracking-wider text-[#ffff] no-underline transition-all duration-200 ease-out md:font-semibold flex items-center gap-1 hover:gap-3">
+            <Link href="#contact" className="bg-gradient-to-r to-pink-500 from-violet-600 p-[1px] rounded-full transition-all duration-300 hover:from-pink-500 hover:to-violet-600">
+              <button className="px-5 font-roboto-mono  md:px-8 py-3 md:py-4 bg-[#0d1224] rounded-full border-none text-center md:text-base font-medium  tracking-wider text-[#ffff] no-underline transition-all duration-200 ease-out  md:font-semibold flex items-center gap-1 hover:gap-3">
                 <span>Contact me</span>
                 <RiContactsFill size={16} />
               </button>
             </Link>
-
-            {/* View Resume Button */}
             <Link
-              className="flex font-roboto-mono items-center gap-1 hover:gap-3 rounded-full bg-gradient-to-r from-pink-500 to-violet-600 px-5 md:px-8 py-3 md:py-4 text-center md:text-base font-medium tracking-wider text-white no-underline transition-all duration-200 ease-out hover:text-white hover:no-underline md:font-semibold"
-              role="button"
-              target="_blank"
-              href={personalData.resume}
-            >
-              <span>View Resume</span>
-              <RiFileTextFill size={16} /> {/* Updated icon */}
-            </Link>
-          </div>
-        </div>
+  className="flex font-roboto-mono items-center gap-1 hover:gap-3 rounded-full bg-gradient-to-r from-pink-500 to-violet-600 px-5 md:px-8 py-3 md:py-4 text-center md:text-base font-medium tracking-wider text-white no-underline transition-all duration-200 ease-out hover:text-white hover:no-underline md:font-semibold"
+  role="button"
+  target="_blank"
+  href={personalData.resume}
+>
+  <span>View Resume</span>
+  <RiFileTextFill size={16} /> {/* Replaced MdDownload with RiFileTextFill */}
+</Link>
 
-        {/* Right Section */}
+
+          </div>
+
+        </div>
         <div className="order-1 lg:order-2 from-[#0d1224] border-[#1b2c68a0] relative rounded-lg border bg-gradient-to-r to-[#0a0d37]">
-          {/* Top Gradient Line */}
           <div className="flex flex-row">
             <div className="h-[1px] w-full bg-gradient-to-r from-transparent via-pink-500 to-violet-600"></div>
             <div className="h-[1px] w-full bg-gradient-to-r from-violet-600 to-transparent"></div>
           </div>
-
-          {/* Browser Controls */}
           <div className="px-4 lg:px-8 py-5">
             <div className="flex flex-row space-x-2">
               <div className="h-3 w-3 rounded-full bg-red-400"></div>
@@ -105,8 +99,6 @@ function HeroSection() {
               <div className="h-3 w-3 rounded-full bg-green-200"></div>
             </div>
           </div>
-
-          {/* Code Snippet Area */}
           <div className="overflow-hidden border-t-[2px] border-indigo-900 px-4 lg:px-8 py-4 lg:py-8">
             <code className="font-mono text-xs md:text-sm lg:text-base">
               <div className="blink">
@@ -121,28 +113,79 @@ function HeroSection() {
                 <span className="text-amber-300">KIXSZH</span>
                 <span className="text-gray-400">{`',`}</span>
               </div>
-              <div>
-                <span className="ml-4 lg:ml-8 mr-2 text-white">skills:</span>
-                <span className="text-gray-400">{`[`}</span>
+              <div className="ml-4 lg:ml-8 mr-2">
+                <span className=" text-white">skills:</span>
+                <span className="text-gray-400">{`['`}</span>
                 <span className="text-amber-300">Python</span>
-                <span className="text-gray-400">{`', '`}</span>
+                <span className="text-gray-400">{"', '"}</span>
+                <span className="text-amber-300">HTML</span>
+                <span className="text-gray-400">{"', '"}</span>
+                <span className="text-amber-300">CSS</span>
+                <span className="text-gray-400">{"', '"}</span>
+                <span className="text-amber-300">React</span>
+                <span className="text-gray-400">{"', '"}</span>
+                <span className="text-amber-300">Express</span>
+                <span className="text-gray-400">{"', '"}</span>
+                <span className="text-amber-300">Java</span>
+                <span className="text-gray-400">{"', '"}</span>
+                <span className="text-amber-300">MySql</span>
+                <span className="text-gray-400">{"', '"}</span>
+                <span className="text-amber-300">MongoDB</span>
+                <span className="text-gray-400">{"', '"}</span>
+                <span className="text-amber-300">Node</span>
+                <span className="text-gray-400">{"', '"}</span>
                 <span className="text-amber-300">NextJS</span>
-                <span className="text-gray-400">{`']`}</span>
+                <span className="text-gray-400">{"'],"}</span>
               </div>
               <div>
-                <span className="ml-4 lg:ml-8 mr-2 text-white">hireable:</span>
+                <span className="ml-4 lg:ml-8 mr-2 text-white">hardWorker:</span>
                 <span className="text-orange-400">true</span>
                 <span className="text-gray-400">,</span>
               </div>
               <div>
-                <span className="text-gray-400">{`}`}</span>
+                <span className="ml-4 lg:ml-8 mr-2 text-white">quickLearner:</span>
+                <span className="text-orange-400">true</span>
+                <span className="text-gray-400">,</span>
               </div>
+              <div>
+                <span className="ml-4 lg:ml-8 mr-2 text-white">problemSolver:</span>
+                <span className="text-orange-400">true</span>
+                <span className="text-gray-400">,</span>
+              </div>
+              <div>
+                <span className="ml-4 lg:ml-8 mr-2 text-green-400">hireable:</span>
+                <span className="text-orange-400">function</span>
+                <span className="text-gray-400">{'() {'}</span>
+              </div>
+              <div>
+                <span className="ml-8 lg:ml-16 mr-2 text-orange-400">return</span>
+                <span className="text-gray-400">{`(`}</span>
+              </div>
+              <div>
+                <span className="ml-12 lg:ml-24 text-cyan-400">this.</span>
+                <span className="mr-2 text-white">hardWorker</span>
+                <span className="text-amber-300">&amp;&amp;</span>
+              </div>
+              <div>
+                <span className="ml-12 lg:ml-24 text-cyan-400">this.</span>
+                <span className="mr-2 text-white">problemSolver</span>
+                <span className="text-amber-300">&amp;&amp;</span>
+              </div>
+              <div>
+                <span className="ml-12 lg:ml-24 text-cyan-400">this.</span>
+                <span className="mr-2 text-white">skills.length</span>
+                <span className="mr-2 text-amber-300">&gt;=</span>
+                <span className="text-orange-400">3</span>
+              </div>
+              <div><span className="ml-8 lg:ml-16 mr-2 text-gray-400">{`);`}</span></div>
+              <div><span className="ml-4 lg:ml-8 text-gray-400">{`};`}</span></div>
+              <div><span className="text-gray-400">{`};`}</span></div>
             </code>
           </div>
         </div>
       </div>
     </section>
   );
-}
+};
 
 export default HeroSection;
